4070ebfdb63e274464506c1b877b3bb4b3f03451
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-07-27 22:10:53 -0400

    Add files via upload
    still trying how to do to render in real time with socket io still i can not figrue it out

--- DisplayCategory.js
+++ DisplayCategory.js
@@ -1,65 +1,9 @@
-// import React from 'react';
-// import axios from 'axios';
-// import { Link } from 'react-router-dom';
-// import { io } from 'socket.io-client';
-
-
-// class DisplayCategory extends React.Component{
-
-//     constructor(props){
-//         super(props);
-//          this.state = {
-//          categories:[],
-//          socket: io('http://localhost:8080')
-//          }
-      
-//       }
-
-//       componentDidMount(){
-//         this.getCategories();
-//       }
-      
-//       getCategories = async ()=>{
-//       await axios.get('http://127.0.0.1:8080/').then((response)=>{
-//         this.setState({categories:response.data.body} , ()=>{ return this.state.categories});
-//       }).catch((error)=>{
-//         console.log(error)
-//       })
-      
-//       //connect our socket to our local server 
-
-
-//     }
-  
-
-   
-      
-//       render(){
-//       return(
-//         <div>
-//       {
-//             this.state.categories.map( element  =>{
-//               return(
-//                  <div key={element.id}>
-                 
-//                 <Link to={`/items/${element.id}`}>{element.categoryName}</Link>
-//                   </div>
-//                 )
-//             })
-//           }
-        
-//           </div>
-//       )
-      
-//       }
-
-// }
 
 // export default DisplayCategory;
 
 import React from 'react';
-
-//import io in the clinet side this is the methid to use
+import axios from 'axios';
+//import io in the client side this is the method to use
 import { io }  from 'socket.io-client';
 class DisplayCategory extends React.Component{
 
@@ -67,32 +11,66 @@ class DisplayCategory extends React.Component{
 state = {
  socket:io('http://127.0.0.1:8080/'),
   text:'',
-  response:''
+  response:'',
+  data:[]
 }
-
-handleSubmit = (event)=>{
-  event.preventDefault();
-  this.state.socket.emit('message' , this.state.text);
-  this.state.socket.on('response' , (message) =>{
-    this.setState({response:message})
+getCategories = async ()=>{
+ await axios.get('http://127.0.0.1:8000/')
+   this.state.socket.on('category-response' , (data)=>{
+    //this.setState({data:data} , ()=>{ return this.state.data})
+    console.log(data);
   })
 }
 
-changeHandler = (event)=>{
-  this.setState({ [event.target.name]: event.target.value } , ()=>{ return this.state.text})
+postCategory = async (category)=>{
+await axios.post('http://127.0.0.1:8000/',{
+  data: category 
+})
+   
+this.state.socket.on('category-added' , (data)=>{
+  console.log(data);
+ })
+
 }
 
+componentDidMount(){this.getCategories();}
+handleSubmit = (event)=>{ 
+  event.preventDefault();
+  this.postCategory(this.state.text);
+
+   
+
+}
+
+
+
+changeHandler = (event)=>{
+this.setState({ [event.target.name]: event.target.value } , ()=>{ return this.state.text})
+}
 
 render(){
 
   return(
     <div>
+      <h1>Add a new Category</h1>
       <h1>{ this.state.response}</h1>
       <form onSubmit={ this.handleSubmit }>
         <input type="text" name="text" className="name" onChange={ this.changeHandler} />
         <button type="submit">Send</button>
       </form>
+      
+      <div>
+        { this.state.data.map( element =>{
+          return(
+            <h1 key={element.id}>{element.categoryName}</h1>
+          )
+        })}
+        </div>
+
       </div>
+
+
+
   )
 
 }
--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// const express = require('express');
-// const getController = require('./controllers/getController');
-// const cors = require('cors');
-// const app = express();
-// //create a server app
-// const server = require('http').createServer(app);
-
-// //require socket.io
-// const socketIo = require('socket.io')(server , {
-//     cors:{ 
-//         origin: "http://localhost:3000/#/",
-//         methods:["GET","POST"]
-//     }
-
-// });
-// //const io = socketIo(server);
-// app.set('view engine' , 'ejs');
-// app.use(express.json());
-
-// app.use(cors({ origin:' * '}));
-// getController(app);
-
-// app.listen( 8080 , ()=>{ console.log('listenning port 8080')});
-
-// //in order to use socket io you must declare the variable server
-// socketIo.on("connection" , (socket)=>{
-//     console.log(socket.id);
-// })
 
 //create the variable express
 const express = require('express');
@@ -35,48 +7,16 @@ const app = express();
 const cors = require('cors');
 //require http server
 const httpServer = require('http');
+const getController = require('./controllers/getController');
+const bodyParser = require('body-parser');
 //create an server http and give app as parameters
 const server = httpServer.createServer(app);
 //set my scket io configuration givin the origin my client and the port is going to listen
-const socketIo = require('socket.io')(8080 , { cors:{
-    origin:['http://localhost:3000'],
-    methods:["GET" , "POST"]
-}})
 
 app.use(express.json());
 app.use(cors())
-
-//use my socket io in with on we listen to a connection and rthen we send a response 
-socketIo.on('connection' , socket =>{
-    console.log(socket.id);
-
-    socket.on('message', (message) =>{
-        console.log(message)
-//with the socketIo variable and the emit we send a message to the client and it can render in real time without refreshing the component
-        socketIo.emit('response', message)
-    })
-})
-
-app.get('/' , (request , response) =>{
-    response.send('<h1>Hello from the server side</h1>')
-})
-
+getController(app);
 app.listen(8000)
 
 
 
-// const socketIo = require('socket.io')(8080 , {cors:{
-//     origin: ['http://localhost:3000']
-// }});
-// //app.use(cors({ origin:' http://localhost:3000/#/ ' , methods:["GET" , "POST"]}));
-// // app.listen(8080 , ()=>{
-// //     console.log('listening in port 8080')
-// // })
-
-// socketIo.on("connection" , (socket)=>{ 
-
-//    socket.on('message' , (message)=>{
-//     socketIo.emit('receive-message' , message) 
-//    })
-
-// })
